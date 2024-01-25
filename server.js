5b2c20cd3e175a5c551f7f711dc960e0ef854fe1
KalyanMarisetti <106854326+KalyanMarisetti@users.noreply.github.com>
2024-01-25 14:14:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,17 +26,17 @@ mongoose.connect(connectionString, {
     .catch((err) => console.error('Connection error:', err));
 
 //Routes 
-app.get('/todo', async (req, res) => {
+app.get('/api/todo', async (req, res) => {
     const todos = await Todo.find();
     res.json(todos);
 });
 
-app.post('/todo/new', async (req, res) => {
+app.post('/api/todo/new', async (req, res) => {
     const newTask = await Todo.create(req.body);
     res.status(201).json({ newTask })
 })
 
-app.delete('/todo/delete/:id', async (req, res) => {
+app.delete('/api/todo/delete/:id', async (req, res) => {
     const result = await Todo.findByIdAndDelete(req.params.id)
     res.json(result)
 })
